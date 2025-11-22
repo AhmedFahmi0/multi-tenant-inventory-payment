e633c4082161e52979e9455e755b4a0f23b5ca9f
AhmedFahmi0 <amfahmi.96@gmail.com>
2025-11-22 15:00:02 +0200

    t push
    Revert "Add inventory and payment READMEs"
    
    This reverts commit 088005ed89e51f76250b25427a30d24fa0717d44.

--- payment/payment/src/main/java/com/payment/model/PaymentMethod.java
+++
@@ -1,7 +0,0 @@
-package com.payment.model;
-
-public enum PaymentMethod {
-    UPI,
-    CARD,
-    NET_BANKING
-}
--- payment/payment/src/main/resources/db/migration/V5__make_email_nullable_in_users.sql
+++
@@ -1,3 +0,0 @@
--- Make email column nullable since authentication is now based on username
-ALTER TABLE users
-    ALTER COLUMN email DROP NOT NULL;
